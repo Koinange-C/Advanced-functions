14096ecabce5a35a047c321db0ba5fba781be2b9
Wanjiru Koinange <koinangewanjiru5@gmail.com>
2026-01-15 14:22:28 +0300

    Add files via upload

---
+++ mrright.c
@@ -0,0 +1,39 @@
+//using string function find() to filter this array down
+#include<stdio.h>
+#include<string.h>
+
+//Looking for mr. right
+//an array of strings that I want to filter down displaying some strings and not displaying others.
+int main(){
+	int NUM_ADS = 7;
+	char *ADS[] = {
+	"William: SBM GSOH  likes sports, TV, dining",
+	"Matt: SWM NS likes, art, movies, theater",
+	"Luis: SLM ND likes books, theater, art",
+	"Mike: DWM DS likes trucks sports and bieber",
+	"Peter: SAM likes chess, working out and art", 
+	"Josh: SJM likes sports, movies and theater",
+	"Jed: DBM likes theater, books and dining"
+	};
+
+	//find() function to track down all the sports fans in the list who dont share a passion for Beiber 
+	void find() {
+		int i;
+		puts("Search results:");
+		puts("------------------------");
+
+		for(i = 0; i < NUM_ADS; i++) {
+			if (strstr(ADS[i], "sports") && ! strstr(ADS[i], "bieber")) {
+ 			printf("%s\n", ADS[i]);	
+
+		}
+
+	}
+	puts("----------------------");
+
+}
+
+find(ADS, NUM_ADS);
+
+return 0;
+}
